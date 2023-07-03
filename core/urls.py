eee5b5693c275aa260315182860192865372e4e3
Hebert Soares <hebertsoaresof@gmail.com>
2023-07-03 15:45:06 -0300

    Corrigindo erro

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('perfil/', include('perfil.urls'))
+    path('perfil/', include('Perfil.urls'))
 ]
