45a24cc3bc796508583d52e6dfa1e3af94a18ae0
Hebert Soares <hebertsoaresof@gmail.com>
2023-07-04 15:50:28 -0300

    Deixando o html mais dinamico

--- Perfil/views.py
+++ Perfil/views.py
@@ -7,7 +7,11 @@ def home(request):
     return render(request, 'home.html')
 
 def gerenciar(request):
-    return render(request, 'gerenciar.html')
+    contas = Conta.objects.all()
+    total_contas = 0
+    for conta in contas:
+        total_contas += conta.valor
+    return render(request, 'gerenciar.html', {'contas': contas,'total_contas': total_contas} )
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -18,7 +22,7 @@ def cadastrar_banco(request):
     
     if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
         messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
-        return redirect('/Perfil/gerenciar/')
+        return redirect('/perfil/gerenciar/')
 
     conta = Conta(
         apelido = apelido,
@@ -30,4 +34,4 @@ def cadastrar_banco(request):
 
     conta.save()
     messages.add_message(request, constants.SUCCESS, 'Conta cadastrada com sucesso')
-    return redirect('/Perfil/gerenciar/')
+    return redirect('/perfil/gerenciar/')
