43111ba5e54bd353d40dfed1a63c2774c250455e
Hebert Soares <hebertsoaresof@gmail.com>
2023-07-05 15:54:31 -0300

    Instale o app e criando  uma URL para extrato

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('perfil/', include('Perfil.urls')),
+    path('extrato/', include('extrato.url')),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
