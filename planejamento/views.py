e639b75a4753380d2ef09fee694c4cf2bb7473b3
Hebert Soares <hebertsoaresof@gmail.com>
2023-07-06 15:00:43 -0300

    Criando a view do definir_planejamento

--- planejamento/views.py
+++ planejamento/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from Perfil.models import Categoria
 
-# Create your views here.
+def definir_planejamento(request):
+    categorias = Categoria.objects.all()
+    return render(request, 'definir_planejamento.html', {'categorias': categorias})
